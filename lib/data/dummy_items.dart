03527f16a331932e74b81abde2f08ae27c1c15b5
rajansurya <rajansurya.88@gmail.com>
2025-04-23 08:25:20 +0530

    named param

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,22 @@
+import 'package:untitledtest/models/Category.dart';
+
+import '../models/GroceryItem.dart';
+import 'categories.dart';
+
+var groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
