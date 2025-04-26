9e2757112ff4ed6d0fac77ea6820c4ae41a9ae29
rajansurya <rajansurya.88@gmail.com>
2025-04-26 23:58:40 +0530

    form validation save form dismissable

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -14,36 +14,56 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems=[];
-  void _addItem() async{
-   final result = await Navigator.of(context).push<GroceryItem>(MaterialPageRoute(builder: (ctx)=>NewItem()));
-   setState(() {
-     _groceryItems.add(result!);
-   });
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final result = await Navigator.of(context)
+        .push<GroceryItem>(MaterialPageRoute(builder: (ctx) => NewItem()));
+    setState(() {
+      _groceryItems.add(result!);
+    });
+  }
+  void _removeItem(int item){
+    setState(() {
+      _groceryItems.removeAt(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content=const Center(child: Text('No item found'),);
+    if(_groceryItems.isNotEmpty){
+      content=ListView.builder(
+
+          itemCount: _groceryItems.length,
+          itemBuilder: (ctx, index) => Dismissible(
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction){
+                _removeItem(index);
+
+            },
+            key: ValueKey(_groceryItems[index].id),
+            child: ListTile(
+              title: Text(
+                _groceryItems[index].name,
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: Colors.amber /*Theme.of(context).primaryColor*/
+                ),
+              ),
+              leading: Container(
+                  width: 20,
+                  height: 20,
+                  color: _groceryItems[index].category.color),
+              trailing: Text(_groceryItems[index].quantity.toString()),
+            ),
+          ));
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Grocery List'),
         actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
       ),
-      body: ListView.builder(
-          itemCount: _groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-                title: Text(
-                  _groceryItems[index].name,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      color: Colors.amber /*Theme.of(context).primaryColor*/
-                      ),
-                ),
-                leading: Container(
-                    width: 20,
-                    height: 20,
-                    color: _groceryItems[index].category.color),
-                trailing: Text(_groceryItems[index].quantity.toString()),
-              )),
+      body: content,
     );
   }
 }
